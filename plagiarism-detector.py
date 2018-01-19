cf67b39de0cef676226ae7a7416f273ec2ceab54
kur <rhdnfka94@gmail.com>
2018-01-19 10:26:52 +0900

    add match score reporting

--- plagiarism-detector.py
+++ plagiarism-detector.py
@@ -16,20 +16,31 @@ strs = [
         (fs1_2,'fs1_2'),
         (fs1_3,'fs1_3'),
         (fs1_4,'fs1_4'),
-        (fs2,'fs2'),
-        (fs3,'fs3'),
-        (fs4,'fs4'),
+        #(fs2,'fs2'),
+        #(fs3,'fs3'),
+        #(fs4,'fs4'),
         (fgeeks,'fgeeks'),
         ]
+
 import itertools
+scores = []
 for pair in itertools.combinations(strs,2):
     (a,b) = pair
     seq1 = sequencer.make_sequence_from(a)
     seq2 = sequencer.make_sequence_from(b)
 
     score = comparator.match_score(seq1,seq2)
+    scores.append( (a,b,score) )
     base_len = min(len(seq1),len(seq2))
     print(a[1] + ' vs ' + b[1] + ' match score is\t\t\t' + str(score))
     print(a[1] + ' vs ' + b[1] + ' /min_len similarity is\t\t' + str(float(score)/float(base_len)))
     print(a[1] + ' vs ' + b[1] + ' /a+b_len * 2 similarity is\t' + str(2 * float(score)/float(len(seq1)+len(seq2))))
     print('------')
+
+for tup in scores:
+    (a,b,score) = tup
+    name_a = a[1]
+    name_b = b[1]
+    
+    print( str(score) + ': ' + name_a + ' vs ' + name_b )
+
