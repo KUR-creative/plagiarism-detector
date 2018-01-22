3d392dc8ee989784f6ca55dc9b2a623210198cf2
kur <rhdnfka94@gmail.com>
2018-01-22 12:54:56 +0900

    remove hardcoded test code. and add plotting histogram of data

--- plagiarism-detector.py
+++ plagiarism-detector.py
@@ -1,46 +1,43 @@
-import preprocessor
-fs4 = preprocessor.get_asm_code('./long/f4.cpp')
-fs3 = preprocessor.get_asm_code('./long/f3.cpp')
-fs2 = preprocessor.get_asm_code('./long/f2.cpp')
-fgeeks = preprocessor.get_asm_code('./long/geeks.cpp')
-fs1_4 = preprocessor.get_asm_code('./long/1-4.cpp')
-fs1_3 = preprocessor.get_asm_code('./long/1-3.cpp')
-fs1_2 = preprocessor.get_asm_code('./long/1-2.cpp')
-fs1_1 = preprocessor.get_asm_code('./long/1-1.cpp')
-s2 = preprocessor.get_asm_code('./long/f2.cpp')
-s1 = preprocessor.get_asm_code('./long/f4.cpp')
+#test_dir = './candle/'
+#test_dir = './eleccar/'
+#test_dir = './food/'
 
-import comparator, sequencer
-strs = [
-        (fs1_1,'fs1_1'),
-        (fs1_2,'fs1_2'),
-        (fs1_3,'fs1_3'),
-        (fs1_4,'fs1_4'),
-        #(fs2,'fs2'),
-        #(fs3,'fs3'),
-        #(fs4,'fs4'),
-        (fgeeks,'fgeeks'),
-        ]
+import os
+import preprocessor, comparator, sequencer
+srcnames = os.listdir(test_dir)
+asm_name_pairs = map(lambda srcname: 
+                        (preprocessor.get_asm_code(test_dir + srcname),
+                         srcname), 
+                     srcnames[:])
+#print(len(asm_name_pairs))
+#print(map(lambda x:x[1], asm_name_pairs))
 
-import itertools
+import itertools 
 scores = []
-for pair in itertools.combinations(strs,2):
+for pair in itertools.combinations(asm_name_pairs,2):
     (a,b) = pair
     seq1 = sequencer.make_sequence_from(a)
     seq2 = sequencer.make_sequence_from(b)
 
     score = comparator.match_score(seq1,seq2)
-    scores.append( (a,b,score) )
-    base_len = min(len(seq1),len(seq2))
-    print(a[1] + ' vs ' + b[1] + ' match score is\t\t\t' + str(score))
-    print(a[1] + ' vs ' + b[1] + ' /min_len similarity is\t\t' + str(float(score)/float(base_len)))
-    print(a[1] + ' vs ' + b[1] + ' /a+b_len * 2 similarity is\t' + str(2 * float(score)/float(len(seq1)+len(seq2))))
-    print('------')
+    scores.append( (score,a,b) )
+    #base_len = min(len(seq1),len(seq2))
+    #print(a[1] + ' vs ' + b[1] + ' match score is\t\t\t' + str(score))
+    #print(a[1] + ' vs ' + b[1] + ' /min_len similarity is\t\t' + str(float(score)/float(base_len)))
+    #print(a[1] + ' vs ' + b[1] + ' /a+b_len * 2 similarity is\t' + str(2 * float(score)/float(len(seq1)+len(seq2))))
+    #print('------')
 
+scores.sort()
 for tup in scores:
-    (a,b,score) = tup
+    (score,a,b) = tup
     name_a = a[1]
     name_b = b[1]
     
     print( str(score) + ': ' + name_a + ' vs ' + name_b )
+
+import matplotlib.pyplot as plt
+#data = [100,200,300,400, 1000,1400,1500,1700]
+plt.hist(map(lambda tup: tup[0], scores), bins = 10, width=10.0)
+#plt.plot(, map(lambda tup: tup[2], scores), 'ro')
+plt.show()
 
